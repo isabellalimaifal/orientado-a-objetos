a7e55280c36dc2098b5cf12568a4f23246b8c44f
isabellalimaifal <iol1@aluno.ifal.edu.br>
2024-09-30 15:46:14 -0300

    Update Animal.js

--- Animal.js
+++ Animal.js
@@ -1,8 +1,8 @@
 class Animal { //classe 
-    nome_especie;
-
-    constructor(nomeAnimal){ //é uma função especial que define como é e como fazer meu objeto
-        console.log("Construindo um animal..");
+    nome_especie; //atributo da classe/armazenar valores
+    
+    constructor(_nome_especie){ //recebe uma variavel
+        this.nome_especie = _nome_especie; //this = pegar os métodos ou atributos da classe
     }
         
     movimentar() { //função
@@ -11,7 +11,7 @@ class Animal { //classe
 }
 
 const cachorro = new Animal(); //'cachorro' vai ser um objeto do tipo animal. 'new Animal();' chama a função 'constructor'
-cachorro.movimentar();
+cachorro.movimentar('cachorro');
 
-const gato = new Animal();
+const gato = new Animal("gato");
 gato.movimentar();
