28bf41b1a8004a5aabe1ae95eb5d0dfa75b84cb9
isabellalimaifal <iol1@aluno.ifal.edu.br>
2024-09-30 16:03:16 -0300

    Update ContaBancaria.js

---
+++ ContaBancaria.js
@@ -0,0 +1,31 @@
+class ContaBancaria {
+    saldo;
+    nomeCorrentista;
+    numeroConta;
+
+    sacar() {
+
+    }
+
+    depositar(valorDeposito) {
+        if(valorDeposito > 0) {
+            this.saldo += 
+            valorDeposito;
+        } else {
+            console.log("Valor inválido!");
+        }
+    }
+
+    mostrarSaldo() {
+        return this.saldo; //retornar o atricuto salvo
+    }
+
+}
+
+const c1 = new ContaBancaria();
+c1.depositar = 10;
+console.log(c1.mostrarSaldo());
+
+const c2 = new ContaBancaria();
+c2.depositar(49);
+console.log(c2.mostrarSaldo());
